bf43696e4b0e96130a3e21cd3e96b607926403b2
bl1nkker <nurovich14@gmail.com>
2023-04-15 16:36:59 +0600

    feat: added db configuration + deployment docker

---
+++ deployment/local/down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose --project-name manuscript-local \
+               --file project/docker-compose.yml \
+               down
--- ms_event/ms_event/settings.py
+++ ms_event/ms_event/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import yaml
+
+with open('app.yaml') as f:
+    database_settings = yaml.safe_load(f)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'ms_event.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": database_settings['DB_ENGINE'],
+        "NAME": database_settings['DB_NAME'],
+        "USER": database_settings['DB_USER'],
+        "PASSWORD": database_settings['DB_PASS'],
+        "HOST": database_settings['DB_HOST'],
+        "PORT": database_settings['DB_PORT'],
     }
 }
 
