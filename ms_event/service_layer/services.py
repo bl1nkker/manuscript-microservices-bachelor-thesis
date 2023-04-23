a7f9a9e2b4fe86c88c61cef9cbc2095a399b8a3e
bl1nkker <nurovich14@gmail.com>
2023-04-23 19:48:05 +0600

    feat: added message publisher on event management events

--- ms_event/service_layer/services.py
+++ ms_event/service_layer/services.py
@@ -24,6 +24,10 @@ def create_event_service(uow: uow.AbstractUnitOfWork, username, **kwargs):
             return Result(data=None, error=exceptions.InvalidEventDataException)
         user = uow.user.get(username=username)
         event = uow.event.create(author=user, **kwargs)
+        try:
+            handle_publish_message_on_event_created(event)
+        except Exception as e:
+            print(e)
         return Result(data=event.to_dict(), error=None)
 
 
@@ -38,6 +42,10 @@ def edit_event_service(uow: uow.AbstractUnitOfWork, id: int, username: str, **kw
         if event.author != user:
             return Result(data=None, error=exceptions.UserIsNotEventAuthorException)
         event = uow.event.edit(id=id, **kwargs)
+        try:
+            handle_publish_message_on_event_edited(event)
+        except Exception as e:
+            print(e)
         return Result(data=event.to_dict(), error=None)
 
 
@@ -50,4 +58,38 @@ def deactivate_event_service(uow: uow.AbstractUnitOfWork, id: int, username: str
         if event.author != user:
             return Result(data=None, error=exceptions.UserIsNotEventAuthorException)
         event = uow.event.deactivate(id=id)
+        try:
+            handle_publish_message_on_event_edited(event)
+        except Exception as e:
+            print(e)
         return Result(data=event.to_dict(), error=None)
+
+
+def handle_publish_message_on_event_created(event):
+    import pika
+    import json
+    from django.conf import settings
+    import services.message_broker as mb
+    credentials = pika.URLParameters(settings.RABBITMQ_CONNECTION_URL)
+    message_broker = mb.RabbitMQ(
+        credentials.host, credentials.port, credentials.credentials.username, credentials.credentials.password, exchange=settings.RABBITMQ_EXCHANGE_NAME)
+    print('Connecting to message broker...')
+    with message_broker:
+        print('Publishing message...')
+        message_broker.publish(
+            message=json.dumps(event.to_dict()), routing_key=settings.RABBITMQ_EVENT_CREATE_ROUTING_KEY)
+
+
+def handle_publish_message_on_event_edited(event):
+    import pika
+    import json
+    from django.conf import settings
+    import services.message_broker as mb
+    credentials = pika.URLParameters(settings.RABBITMQ_CONNECTION_URL)
+    message_broker = mb.RabbitMQ(
+        credentials.host, credentials.port, credentials.credentials.username, credentials.credentials.password, exchange=settings.RABBITMQ_EXCHANGE_NAME)
+    print('Connecting to message broker...')
+    with message_broker:
+        print('Publishing message...')
+        message_broker.publish(
+            message=json.dumps(event.to_dict()), routing_key=settings.RABBITMQ_EVENT_EDIT_ROUTING_KEY)
