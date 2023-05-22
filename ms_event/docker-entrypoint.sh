e0e0211bf02e6a485d989abd71f4cce668439d81
bl1nkker <nurovich14@gmail.com>
2023-05-22 20:39:50 +0600

    fix: added entrypoint for event consumer

--- ms_event/docker-entrypoint.sh
+++ ms_event/docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-./wait-for-it.sh ms_event_db:5432 -- python manage.py migrate
-exec gunicorn ms_event.wsgi:application -c gunicorn.conf.py
+# Check the value of the SERVICE_TYPE environment variable
+if [ "$SERVICE_TYPE" = "service" ]; then
+  # Run the gunicorn command
+  ./wait-for-it.sh ms_event_db:5432 -- python manage.py migrate
+  exec gunicorn ms_event.wsgi:application -c gunicorn.conf.py
+elif [ "$SERVICE_TYPE" = "consumer" ]; then
+  # Run the event consumer command
+  ./wait-for-it.sh ms_event:16010 --
+  ./wait-for-it.sh rabbitmq:5672 -- python -m entrypoints.event_consumer
+fi
